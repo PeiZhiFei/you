1a763b110219a4ef9097424b062dd24d52e6b77f
fei <peizhifei@meituan.com>
2024-11-29 10:46:03 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import Screen3 from "./Screen3.tsx";
+import Test from "./Test.tsx";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Layout} from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Screen3/>
-      </header>
-    </div>
+      <Router >
+          <Layout>
+              <Switch>
+                  <Route exact path="/home" component={Screen3} />
+                  <Route exact path="/s" component={Screen3} />
+                  <Route path="/download#*" component={Screen3} />
+                  <Route path="/work/:source" component={Screen3} />
+                  <Route path="*" component={Screen3} />
+              </Switch>
+          </Layout>
+      </Router>
   );
 }
 
